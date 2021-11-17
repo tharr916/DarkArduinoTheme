c8e7a46e156a0be08cb59301c340f87a07283fa8
Timothy Harrington <tharrington916@gmail.com>
2021-11-17 18:14:47 -0500

    add a basic ino file for screenshots

---
+++ repo_og_img.source/repo_og_img.source.ino
@@ -0,0 +1,15 @@
+// a comment
+/*  another comment */
+// url: https://tharr916.com
+
+int i = 100;
+char c = 'c';
+String s = "a string";
+boolean b = true;
+
+void setup() {
+  pinMode(13, INPUT);
+  if (b == true) {
+    s="hello world"
+  }
+}
